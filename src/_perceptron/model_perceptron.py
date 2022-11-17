545720e7fab7db7822ebc4aa56a38488c4f6b0b8
Karyn Talyne <66790033+KarynTalyne@users.noreply.github.com>
2022-11-16 22:42:24 -0300

    clear code

--- src/_perceptron/model_perceptron.py
+++ src/_perceptron/model_perceptron.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from _data import ImportSQL
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import average_precision_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
